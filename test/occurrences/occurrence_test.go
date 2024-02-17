3016bceee96024cee1b2307864deeb8a9cdb271a
Matheus Lima <matheus.limastack@outlook.com>
2024-02-17 16:42:13 -0300

    test: fix occurrence test

--- test/occurrences/occurrence_test.go
+++ test/occurrences/occurrence_test.go
@@ -8,12 +8,14 @@ import (
 	occurrenceGrpc "buf.build/gen/go/matheusslima/go-poc/protocolbuffers/go/occurrences"
 	grpcServer "github.com/mateusfdl/go-poc/internal/grpc"
 	"github.com/mateusfdl/go-poc/internal/logger"
+	"github.com/mateusfdl/go-poc/internal/mongo"
 	"github.com/mateusfdl/go-poc/internal/occurrences"
-	grpcInternal "github.com/mateusfdl/go-poc/internal/occurrences/grpc"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"go.uber.org/fx"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 var (
@@ -21,42 +23,47 @@ var (
 		occurrences.Module,
 		grpcServer.Module,
 		logger.Module,
+		mongo.Module,
 	)
 
 	clientConn       *grpc.ClientConn
 	OccurrenceClient occurrencesgrpc.OccurrenceServiceClient
 )
 
-func TestOrderGRPCHandler(t *testing.T) {
+func TestOccurrenceRPCHandler(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Order GRPC handler suite")
+	RunSpecs(t, "Occurrence rpc call")
 }
 
 var _ = BeforeSuite(func() {
 	Expect(TestApp.Start(context.Background())).To(Succeed())
 
-	//var dialErr error
-	//clientConn, dialErr = grpc.Dial(
-	//	"",
-	//	grpc.WithTransportCredentials(insecure.NewCredentials()),
-	//)
+	var dialErr error
+	clientConn, dialErr = grpc.Dial(
+    "localhost:8080",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 
-	//Expect(dialErr).NotTo(HaveOccurred())
+	Expect(dialErr).NotTo(HaveOccurred())
 
-	//OccurrenceClient = occurrencesgrpc.NewOccurrenceServiceClient(clientConn)
+	OccurrenceClient = occurrencesgrpc.NewOccurrenceServiceClient(clientConn)
 })
 
 var _ = AfterSuite(func() {
-	//Expect(clientConn.Close()).To(Succeed())
+	Expect(clientConn.Close()).To(Succeed())
 	Expect(TestApp.Stop(context.Background())).To(Succeed())
 })
 
 var _ = Describe("grpc occurrence handler", func() {
 	Describe("CreateOccurrence", func() {
-		It("sends a create event to the creation producer channel", func() {
+		It("creates a occurrence", func() {
 			resp, err := OccurrenceClient.CreateOccurrence(
 				context.Background(),
-				&occurrenceGrpc.NewOccurrence{},
+				&occurrenceGrpc.NewOccurrence{
+					OccurrenceCode: 0,
+					OccurrenceTime: timestamppb.Now(),
+					UserId:         "65cbcd82f5cec8b2f2b1b29f",
+				},
 			)
 			Expect(err).To(Equal(nil))
 
